c60b9b7e55ffdbce7788558d8addada133ab0db6
Evgenii Vlasov <senzik@gmail.com>
2023-07-20 16:19:50 +0300

    Первая версия выполненного задания

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class BaseTest {
+
+    @Test
+    void printFirst() {
+        System.out.println("First");
+    }
+
+    @Test
+    void printSecond() {
+        System.out.println("Second");
+    }
+}
